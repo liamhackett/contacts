e40b989f93f57b29742a54b4876f62a1b9dacce2
Liam Hackett <ljh1oo@yahoo.com>
2023-09-11 16:47:36 -0400

    Updated project

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import './SearchBox.css';
 const SearchBox = ({searchChange}) => {
     return (
         <div className="pa2">
             <input 
-                className="pa3 ba b--gray bg-lightest-blue"
+                className="pa3 ba b--gray .search-bar"
                 type="search" 
                 placeholder="Search" 
                 onChange={searchChange}
